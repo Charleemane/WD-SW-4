1f90b51633c83f311a1bba39e1a01c0d717c877f
Charleemane <selrahceed@gmail.com>
2024-07-23 17:49:38 +0800

    refactor(landing-page): added animation and function for awards.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,9 @@
-console.log("inside seperate file");
+document.getElementById("myImg");
+var link = document.getElementById("myImg");
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
-var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myHeaderFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+function clickImg() {
+  window.location = "../../pages/feature-1/index.html"
+  if (link.onclick) {
+    window.open(window.location)
   }
 }
