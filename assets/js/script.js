93e669c368c0978adc99932f3068ffc411ea6da7
Charleemane <selrahceed@gmail.com>
2024-07-28 11:17:48 +0800

    fix(clickImg): changed file path to resolve issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,19 @@
-var link = document.getElementById("myImg");
-
 function clickImg1() {
-  window.location = "../../pages/feature-1/index.html#section-1";
+  window.location = "./pages/feature-1/index.html#section-1";
 }
 
 function clickImg2() {
-  window.location = "../../pages/feature-1/index.html#section-2";
+  window.location = "./pages/feature-1/index.html#section-2";
 }
 
 function clickImg3() {
-  window.location = "../../pages/feature-1/index.html#section-3";
+  window.location = "./pages/feature-1/index.html#section-3";
 }
 
 function clickImg4() {
-  window.location = "../../pages/feature-1/index.html#section-4";
+  window.location = "./pages/feature-1/index.html#section-4";
 }
 
 function clickImg5() {
-  window.location = "../../pages/feature-1/index.html#section-5";
+  window.location = "./pages/feature-1/index.html#section-5";
 }
