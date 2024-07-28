a83a768bb38c7db6d09d2da528f83aa81cb580f2
Charleemane <selrahceed@gmail.com>
2024-07-28 10:50:15 +0800

    fix(clickImg): attempt to fix script to open feature 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,21 @@
-document.getElementById("myImg");
 var link = document.getElementById("myImg");
 
 function clickImg1() {
-  window.location = "../../pages/feature-1/index.html#section-1"
-  if (link.onclick) {
-    window.open(window.location)
-  }
+  window.location = "../../pages/feature-1/index.html#section-1";
 }
 
 function clickImg2() {
-  window.location = "../../pages/feature-1/index.html#section-2"
-  if (link.onclick) {
-    window.open(window.location)
-  }
+  window.location = "../../pages/feature-1/index.html#section-2";
 }
 
 function clickImg3() {
-  window.location = "../../pages/feature-1/index.html#section-3"
-  if (link.onclick) {
-    window.open(window.location)
-  }
+  window.location = "../../pages/feature-1/index.html#section-3";
 }
 
 function clickImg4() {
-  window.location = "../../pages/feature-1/index.html#section-4"
-  if (link.onclick) {
-    window.open(window.location)
-  }
+  window.location = "../../pages/feature-1/index.html#section-4";
 }
 
 function clickImg5() {
-  window.location = "../../pages/feature-1/index.html#section-5"
-  if (link.onclick) {
-    window.open(window.location)
-  }
+  window.location = "../../pages/feature-1/index.html#section-5";
 }
